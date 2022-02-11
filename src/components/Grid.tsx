b1cce6add5f49c5e139e829d5df7b44d79a1bf8c
Ngoc Nguyen Duy Minh <minhngocnguyenduy@gmail.com>
2022-02-11 15:56:22 +0700

    Fix: Change Grid.tsx
    Remove unused type from `GridProps`

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Grid.scss'
 
-type GridProps = React.HTMLProps<HTMLDivElement> & {}
+type GridProps = React.HTMLProps<HTMLDivElement>
 
 const Grid: React.FC<GridProps> = ({ children, ...rest }) => {
   return (
