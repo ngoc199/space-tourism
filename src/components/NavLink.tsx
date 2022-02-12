a6f652354662252ca6afd77207c79ad8f0ae6359
Ngoc Nguyen Duy Minh <minhngocnguyenduy@gmail.com>
2022-02-12 12:38:49 +0700

    Fix: Navlink component
    Change `NavLinkProps` to get `li` element props too.

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './NavLink.scss'
 
-type NavLinkProps = {
+type NavLinkProps = React.HTMLProps<HTMLLIElement> & {
   href: string
   active?: boolean
   linkNumber?: number
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ linkNumber, active, children, href }) => {
+const NavLink: React.FC<NavLinkProps> = ({ linkNumber, active, children, href, ...rest }) => {
 
   return (
-    <li className={'text-nav nav__item text--light ' + (active ? 'nav__item--active' : '')}>
+    <li {...rest} className={'text-nav nav__item text--light ' + (active ? 'nav__item--active' : '')}>
       <span className='nav__item__number'>{linkNumber !== undefined && linkNumber.toString().padStart(2, '0')}</span>
       <Link to={href}>{children}</Link>
     </li>
