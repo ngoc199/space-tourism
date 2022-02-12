faaec01daafe101782bf42bbc926d659b8ff4544
Ngoc Nguyen Duy Minh <minhngocnguyenduy@gmail.com>
2022-02-12 12:39:19 +0700

    Fix: Navbar
    Change styles of navbar to responsive on mobile and desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,36 @@ import { Link } from 'react-router-dom'
 type NavbarProps = {}
 
 const Navbar: React.FC<NavbarProps> = () => {
+
+  const [open, setOpen] = React.useState(false)
+
+  const handleMenuClick = () => {
+    setOpen(prevState => !prevState)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   return (
-    <header className='navbar'>
+    <header className='navbar mt-10'>
 
       <Link className='navbar__logo' to='/'><img src="/img/shared/logo.svg" alt="Space Tourism Logo" /></Link>
 
       <hr className='navbar__line' />
 
-      <nav>
-        <ul className='navbar__links'>
-          <NavLink linkNumber={0} href='/'>Home</NavLink>
-          <NavLink linkNumber={1} href='/destination'>Destination</NavLink>
-          <NavLink linkNumber={2} href='/crew'>Crew</NavLink>
-          <NavLink linkNumber={3} href='/technology'>Technology</NavLink>
+      <nav className='navbar__nav'>
+        <ul className={'nav__links ' + (open ? 'nav__links--open' : '')}>
+          <NavLink linkNumber={0} href='/' onClick={handleClose}>Home</NavLink>
+          <NavLink linkNumber={1} href='/destination' onClick={handleClose}>Destination</NavLink>
+          <NavLink linkNumber={2} href='/crew' onClick={handleClose}>Crew</NavLink>
+          <NavLink linkNumber={3} href='/technology' onClick={handleClose}>Technology</NavLink>
         </ul>
       </nav>
+
+      <div className='navbar__menu-button' onClick={handleMenuClick}>
+        {open ? <img src="/img/shared/icon-close.svg" alt="Close Menu Button" /> : <img src="/img/shared/icon-hamburger.svg" alt="Menu Button" />}
+      </div>
     </header>
   )
 }
