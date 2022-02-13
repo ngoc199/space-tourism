89a977f53c63350569b476909d057ef84313dda6
Ngoc Nguyen Duy Minh <minhngocnguyenduy@gmail.com>
2022-02-13 14:34:35 +0700

    Add: Technology page
    Add technology page.

--- src/components/SliderCircleButton.tsx
+++ src/components/SliderCircleButton.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 type SliderCircleButtonProps = {
   active?: boolean
+  onClick: (value: any) => void
 }
 
-const SliderCircleButton: React.FC<SliderCircleButtonProps> = ({ children, active }) => {
+const SliderCircleButton: React.FC<SliderCircleButtonProps> = ({ children, active, onClick }) => {
   return (
-    <button className={'slider-circle-button ' + (active ? 'slider-circle-button--active' : '')}>{children}</button>
+    <button className={'slider-circle-button ' + (active ? 'slider-circle-button--active' : '')} onClick={onClick}>{children}</button>
   )
 }
 
